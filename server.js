7ad847681c70eeec38f1fe2643784f5d8098877b
Samardeep Kajal <samk712003@gmail.com>
2023-07-24 23:43:53 +0530

    Added backend starter code

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const connectDB = require("./backend/config/db");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./backend/middleware/errorMiddleware");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(errorHandler);
+
+const port = process.env.PORT || 5050;
+app.listen(port, () => console.log(`Server started on port ${port}`));
