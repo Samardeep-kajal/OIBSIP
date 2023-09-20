009aebbd8cf904ba65e97a0d55db4b4d85d0b8fd
Samardeep Kajal <samk712003@gmail.com>
2023-09-20 20:50:16 +0530

    added minor fixes

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(errorHandler);
 
 app.use("/api/user", require("./backend/routes/userRoutes"));
 app.use("/api/admin", require("./backend/routes/adminRoutes"));
+app.use("/api/pizza", require("./backend/routes/pizzaRoute"));
 
 const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`Server started on port ${port}`));
