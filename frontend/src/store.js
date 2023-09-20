acdc2cff9a5bdb35158c7123f993e0ddd4293e50
Samardeep Kajal <samk712003@gmail.com>
2023-09-20 20:36:03 +0530

    redux store setup for all pizza

---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { getAllPizzaReducer } from "./reducers/pizzaReducer";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+const rootReducer = combineReducers({
+  getAllPizzaReducer: getAllPizzaReducer,
+});
+const initialState = {};
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+export default store;
