91b2fe376fd00726b665b658b5bb54ade0427fde
Samardeep Kajal <samk712003@gmail.com>
2023-09-26 16:43:42 +0530

    Fixed delete from cart action

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,3 +1,15 @@
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+
+  return true;
+}
+
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
@@ -20,12 +32,24 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
         };
       }
     case "DELETE_FROM_CART":
-      return {
-        ...state,
-        cartItems: state.cartItems.filter(
-          (item) => item._id !== action.payload._id
-        ),
-      };
+      const { _id, size } = action.payload;
+
+      const indexToRemove = state.cartItems.findIndex(
+        (item) => item._id === _id && item.size === size
+      );
+
+      if (indexToRemove !== -1) {
+        const updateCartItems = [
+          ...state.cartItems.slice(0, indexToRemove),
+          ...state.cartItems.slice(indexToRemove + 1),
+        ];
+        return {
+          ...state,
+          cartItems: updateCartItems,
+        };
+      } else {
+        return state;
+      }
     default:
       return state;
   }
