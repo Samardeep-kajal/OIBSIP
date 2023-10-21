016776b4d8d71853c61ba22d3167d8e477817bef
Samardeep Kajal <samk712003@gmail.com>
2023-10-21 17:25:13 +0530

    Added action for order and stripe checkout

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -6,13 +6,18 @@ export const placeOrder = (token, subtotal) => async (dispatch, getState) => {
   const cartItems = getState().cartReducer.cartItems;
 
   try {
-    const res = await axios.post("/api/orders/placeorder", {
+    const res = await axios.post("http://localhost:5005/api/order/placeorder", {
       token,
       subtotal,
       currentUser,
       cartItems,
     });
-    dispatch({ type: "PLACE_ORDER_SUCCESS" });
+    if (res.data.message === "Payment Success") {
+      window.location.href = res.data.redirectUrl;
+      dispatch({ type: "PLACE_ORDER_SUCCESS" });
+    } else {
+      dispatch({ type: "PLACE_ORDER_FAIL" });
+    }
     console.log(res);
   } catch (error) {
     dispatch({ type: "PLACE_ORDER_FAIL" });
